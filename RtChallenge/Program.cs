9ef00860b4f0d6050a8c3b106e51e4f9c7657f99
Kyle Coffey <me@phytolizer.dev>
2022-08-03 18:34:08 -0500

    Play with the tuples

--- RtChallenge/Program.cs
+++ RtChallenge/Program.cs
@@ -4,6 +4,15 @@ internal static class Program
 {
     private static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        var p = new Projectile(Tuple.Point(0, 1, 0), Tuple.Vector(1, 1, 0).Normalize());
+        var e = new Environment(Tuple.Vector(0, -0.1, 0), Tuple.Vector(-0.01, 0, 0));
+        var ticksAlive = 0;
+        while (p.Position.Y > 0)
+        {
+            Console.WriteLine($"{p.Position.X:0.0000} {p.Position.Y:0.0000} {p.Position.Z:0.0000}");
+            p = e.Tick(p);
+            ticksAlive++;
+        }
+        Console.WriteLine($"Landed in {ticksAlive} ticks");
     }
 }
