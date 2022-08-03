b90f72fde5fd1f9aebdbb02bdf3c543319b895b7
Kyle Coffey <me@phytolizer.dev>
2022-08-03 17:45:23 -0500

    use an explicit class

--- RtChallenge/Program.cs
+++ RtChallenge/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace RtChallenge;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
